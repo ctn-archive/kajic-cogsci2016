c06a2813f58b5e9b41d1eddc7e4d90c3eb6ee797
Ivana Kajic <ivana.kajic@gmail.com>
2016-01-14 11:45:58 -0500

    Added first similarity analyses

--- src/vector_similarities/compute_similarity.py
+++ src/vector_similarities/compute_similarity.py
@@ -44,51 +44,55 @@ def all_words_in_vocab(problem, vocabulary):
 
     return word_in_vocabulary
 
+def compute_similarity(method):
+    # load RAT items as a list of lists
+    rat_problems = load_rat_problems()
+
+    # load word vocabulary as list and vectors as an ndarray
+    vectors, w2i, i2w = load_vectors(method)
+
+    similarities_target, similarities_everything = [], []
+
+    nr_problems = 0
+    total_problems = len(rat_problems)
+
+    for problem in rat_problems:
+        # check the RAT words exist in the vocabulary
+        if not all_words_in_vocab(problem, w2i.keys()):
+            continue
+
+        nr_problems += 1
+
+        c1, c2, c3, target = problem
+
+        # get vectors for cues and target
+        cue_vectors = vectors[[w2i[c1], w2i[c2], w2i[c3]]]
+        target_vector = vectors[w2i[target]]
+
+        # cues -> target similarity
+        cue_target_sim = cos_sim(cue_vectors, target_vector)
+        similarities_target.append(cue_target_sim)
+
+        # cues -> all other words similarity
+        cues_words_sim = [cos_sim(cue_vectors[0], vectors).mean(),
+                          cos_sim(cue_vectors[1], vectors).mean(),
+                          cos_sim(cue_vectors[2], vectors).mean()]
+        similarities_everything.append(cues_words_sim)
+
+    sim_target = np.array(similarities_target, dtype=np.float)
+    sim_everything = np.array(similarities_everything, dtype=np.float)
+
+    print('%d/%d problems exist with the %s vocabulary.' % (nr_problems,
+          total_problems, method))
+    print('Average similarity with the target: %.5f (std=%.3f)' %
+          (sim_target.mean(), sim_target.std()))
+    print('Average similarity with all words: %.5f (std=%.3f)' %
+          (sim_everything.mean(), sim_everything.std()))
+    return sim_target, sim_everything
 
 
-method = 'random_483w_16d'
-method = 'svd_5018w_16d'
+if __name__ == '__main__':
+    method = 'svd_5018w_256d'
+    
+    st, se = compute_similarity(method)
 
-# load RAT items as a list of lists
-rat_problems = load_rat_problems()
-
-# load word vocabulary as list and vectors as an ndarray
-vectors, w2i, i2w = load_vectors(method)
-
-similarities_target, similarities_everything = [], []
-
-nr_problems = 0
-total_problems = len(rat_problems)
-
-for problem in rat_problems:
-    # check the RAT words exist in the vocabulary
-    if not all_words_in_vocab(problem, w2i.keys()):
-        continue
-
-    nr_problems += 1
-
-    c1, c2, c3, target = problem
-
-    # get vectors for cues and target
-    cue_vectors = vectors[[w2i[c1], w2i[c2], w2i[c3]]]
-    target_vector = vectors[w2i[target]]
-
-    # cues -> target similarity
-    cue_target_sim = cos_sim(cue_vectors, target_vector)
-    similarities_target.append(cue_target_sim)
-
-    # cues -> all other words similarity
-    cues_words_sim = [cos_sim(cue_vectors[0], vectors).mean(),
-                      cos_sim(cue_vectors[1], vectors).mean(),
-                      cos_sim(cue_vectors[2], vectors).mean()]
-    similarities_everything.append(cues_words_sim)
-
-sim_target = np.array(similarities_target, dtype=np.float)
-sim_everything = np.array(similarities_everything, dtype=np.float)
-
-print('%d/%d problems exist with the %s vocabulary.' % (nr_problems,
-      total_problems, method))
-print('Average similarity with the target: %.5f (std=%.3f)' %
-      (sim_target.mean(), sim_target.std()))
-print('Average similarity with all words: %.5f (std=%.3f)' %
-      (sim_everything.mean(), sim_everything.std()))
