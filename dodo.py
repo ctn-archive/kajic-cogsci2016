ae974a323ea4c33facc8c298c285a53374f6744b
Jan Gosmann <jgosmann@uwaterloo.ca>
2016-01-22 14:08:54 -0500

    Update doit file.

--- dodo.py
+++ dodo.py
@@ -3,66 +3,83 @@ import os.path
 from src.datasets import datasets
 
 
-def task_fetch_free_association_data():
-    return {
-        'actions': ['scripts/fetch_data.py freeassoc'],
-        'targets': [
-            os.path.join('data', 'raw', 'freeassoc', file_['name'])
-            for file_ in datasets['freeassoc']['files']],
-        'uptodate': [True],  # only download data if not existent
-    }
+def assocmat_files(name):
+    assocdir = os.path.join('data', 'associationmatrices')
+    return [os.path.join(assocdir, name + ext) for ext in ['.npy', '.pkl']]
 
 
-def task_fetch_google_data():
-    return {
-        'actions': ['scripts/fetch_data.py google'],
-        'targets': [
-            os.path.join('data', 'raw', 'google', file_['name'])
-            for file_ in datasets['freeassoc']['files']],
-        'uptodate': [True],  # only download data if not existent
-    }
+def sp_files(name):
+    spdir = os.path.join('data', 'semanticpointers')
+    return [os.path.join(spdir, name + ext) for ext in ['.npy', '.pkl']]
 
 
-def task_gen_symmetric_association_matrix():
-    return {
+def task_fetch_data():
+    for name, dataset in datasets.items():
+        yield {
+            'name': name,
+            'actions': ['scripts/fetch_data.py ' + name],
+            'targets': [
+                os.path.join('data', 'raw', name, file_['name'])
+                for file_ in dataset['files']],
+            'uptodate': [True],  # only download data if not existent
+        }
+
+
+def task_gen_association_matrices():
+    matrices = [
+        ('freeassoc', 'symmetric'),
+        ('freeassoc', 'asymmetric'),
+        ('google', '1grams'),
+        ('google', 'bigrams')]
+    for dataset, method in matrices:
+        yield {
+            'name': dataset + '_' + method,
+            'actions': [
+                'scripts/generate_association_matrix.py {0} {1}'.format(
+                    dataset, method)],
+            'file_dep': [
+                os.path.join('data', 'raw', dataset, file_['name'])
+                for file_ in datasets[dataset]['files']],
+            'targets': assocmat_files(dataset + '_' + method),
+        }
+
+    yield {
+        'name': 'google_combined',
         'actions': [
-            'scripts/generate_association_matrix.py freeassoc symmetric'],
-        'file_dep': [
-            os.path.join('data', 'raw', 'freeassoc', file_['name'])
-            for file_ in datasets['freeassoc']['files']],
-        'targets': [
-            os.path.join(
-                'data', 'associationmatrices', 'freeassoc_symmetric' + ext)
-            for ext in ['.npy', '.pkl']],
+            'scripts/transform_association_matrix.py add google_combined '
+            'google_bigrams google_1grams'],
+        'file_dep': assocmat_files('google_bigrams') + assocmat_files(
+            'google_1grams'),
+        'targets': assocmat_files('google_combined'),
     }
 
-
-def task_gen_asymmetric_association_matrix():
-    return {
+    yield {
+        'name': 'google_normalized',
         'actions': [
-            'scripts/generate_association_matrix.py freeassoc asymmetric'],
-        'file_dep': [
-            os.path.join('data', 'raw', 'freeassoc', file_['name'])
-            for file_ in datasets['freeassoc']['files']],
-        'targets': [
-            os.path.join(
-                'data', 'associationmatrices', 'freeassoc_asymmetric' + ext)
-            for ext in ['.npy', '.pkl']],
+            'scripts/transform_association_matrix.py normalize '
+            'google_normalized google_combined'],
+        'file_dep': assocmat_files('google_combined'),
+        'targets': assocmat_files('google_normalized'),
     }
 
-
-def task_gen_semantic_pointers():
-    return {
+    yield {
+        'name': 'google_symmetric',
         'actions': [
-            'scripts/generate_semantic_pointers.py freeassoc_symmetric '
-            'svd_factorize 256'],
-        'file_dep': [
-            os.path.join(
-                'data', 'associationmatrices', 'freeassoc_symmetric' + ext)
-            for ext in ['.npy', '.pkl']],
-        'targets': [
-            os.path.join(
-                'data', 'semanticpointers',
-                'freeassoc_symmetric_svd_factorize_256w_256d' + ext)
-            for ext in ['.npy', '.pkl']],
+            'scripts/transform_association_matrix.py symmetrify '
+            'google_symmetric google_normalized'],
+        'file_dep': assocmat_files('google_normalized'),
+        'targets': assocmat_files('google_symmetric'),
     }
+
+
+def task_svd():
+    for assocmat in ['freeassoc_symmetric', 'google_symmetric']:
+        for d in [4096, 3062, 2048, 1024, 768, 512, 380, 256, 128]:
+            yield {
+                'name': assocmat + '_' + str(d),
+                'actions': [
+                    'scripts/generate_semantic_pointers.py {0} svd_factorize '
+                    '{1}'.format(assocmat, d)],
+                'file_dep': assocmat_files(assocmat),
+                'targets': sp_files(assocmat + '_5018w_' + str(d) + 'd'),
+            }
