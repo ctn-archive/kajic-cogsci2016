a27b4a341eeaa717ca073cd5ab8e44e166d65cf8
Jan Gosmann <jgosmann@uwaterloo.ca>
2016-01-24 14:43:55 -0500

    Add ffwd model with connection based associations.

--- src/model/benchmark.py
+++ src/model/benchmark.py
@@ -12,6 +12,7 @@ import ctn_benchmark
 import nengo
 from nengo import spa
 
+from data_processing.generate_association_matrix import load_assoc_mat
 from data_processing.spgen import load_pointers
 from data_processing.rat import load_rat_items
 from model.stimulus import filter_valid, Stimulus, StimulusModule
@@ -62,6 +63,49 @@ class RatModel(ctn_benchmark.Benchmark):
             output=sim.data[self.p_output],
             rat_items=[x.id for x in self.rat_items])
 
+
+class ConnectionsRatModel(ctn_benchmark.Benchmark):
+    def params(self):
+        self.default("number of dimensions", d=256)
+        self.default(
+            "association matrix", assocmat='freeassoc_asymmetric')
+        self.default("rat problems", ratfile='example.txt')
+        self.default("experiment seed", exp_seed=23)
+        self.default("model seed", model_seed=42)
+
+    def model(self, p):
+        random.seed(p.exp_seed)
+
+        data_dir = os.path.join(
+            os.path.dirname(__file__), os.pardir, os.pardir, 'data')
+        sp_path = os.path.join(data_dir, 'associationmatrices')
+        pointers, i2w, _ = load_assoc_mat(sp_path, p.assocmat)
+
+        rat_path = os.path.join(data_dir, 'rat', p.ratfile)
+        self.rat_items = list(filter_valid(load_rat_items(rat_path), i2w))
+
+        with spa.SPA(seed=p.model_seed) as model:
+            # set up vocab
+            vocab = model.get_default_vocab(p.d)
+            for i, pointer in enumerate(pointers):
+                vocab.add(i2w[i].upper(), pointer)
+
+            # set up model
+            self.stimulus = Stimulus(self.rat_items)
+            model.stimulus = StimulusModule(self.stimulus, p.d)
+            model.rat_model = FfwdRat(p.d)
+            nengo.Connection(model.stimulus.cue1.output, model.rat_model.cue1)
+            nengo.Connection(model.stimulus.cue2.output, model.rat_model.cue2)
+            nengo.Connection(model.stimulus.cue3.output, model.rat_model.cue3)
+            self.p_output = nengo.Probe(model.rat_model.rat_state.output)
+
+        return model
+
+    def evaluate(self, p, sim, plt):
+        sim.run(self.stimulus.total_duration)
+        return dict(
+            output=sim.data[self.p_output],
+            rat_items=[x.id for x in self.rat_items])
 
 if __name__ == '__main__':
     RatModel().run()
--- src/model/ffwd.py
+++ src/model/ffwd.py
@@ -1,5 +1,7 @@
 from __future__ import division
 
+import numpy as np
+
 import nengo
 from nengo import spa
 
@@ -27,6 +29,39 @@ class FfwdRat(spa.module.Module):
             nengo.Connection(
                 self.cue3, self.rat_state.input, synapse=None,
                 transform=1/3.)
+
+        self.inputs = dict(
+            cue1=(self.cue1, vocab),
+            cue2=(self.cue1, vocab),
+            cue3=(self.cue1, vocab))
+        self.outputs = dict(default=(self.rat_state.output, vocab))
+
+
+class FfwdConnectionsRat(spa.module.Module):
+    def __init__(
+            self, assoc, d, vocab, label=None, seed=None,
+            add_to_container=None):
+        super(FfwdConnectionsRat, self).__init__(label, seed, add_to_container)
+
+        assoc = np.copy(assoc)
+        assoc.fill_diagonal(0.)
+        tr = np.dot(vocab.vectors.T, np.dot(assoc.T, vocab.vectors)) / 3.
+
+        with self:
+            self.cue1 = nengo.Node(size_in=d)
+            self.cue2 = nengo.Node(size_in=d)
+            self.cue3 = nengo.Node(size_in=d)
+            self.rat_state = spa.State(d, vocab=vocab)
+
+            nengo.Connection(
+                self.cue1, self.rat_state.input, synapse=None,
+                transform=tr)
+            nengo.Connection(
+                self.cue2, self.rat_state.input, synapse=None,
+                transform=tr)
+            nengo.Connection(
+                self.cue3, self.rat_state.input, synapse=None,
+                transform=tr)
 
         self.inputs = dict(
             cue1=(self.cue1, vocab),
